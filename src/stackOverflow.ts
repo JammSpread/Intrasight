015ebee096ce65e51545d3c0a95069356a246f5f
JK <u.sslooky@gmail.com>
2021-02-08 04:53:51 +0000

    src/stackOverflow.ts: Refactor Refactor stackOverflow.ts + add .prettierrc.yaml to .vscodeignore.
    Signed-off-by: JK <u.sslooky@gmail.com>

--- src/stackOverflow.ts
+++ src/stackOverflow.ts
@@ -5,11 +5,7 @@ import * as zlib from "zlib"
 
 export class StackOverflowProvider
 	implements vscode.TreeDataProvider<StackOverflowModel> {
-	userQuery: string
-
-	constructor(userQuery: string) {
-		this.userQuery = userQuery
-	}
+	constructor(public readonly userQuery: string) {}
 
 	getTreeItem(
 		element: StackOverflowModel,
@@ -18,61 +14,56 @@ export class StackOverflowProvider
 	}
 
 	getChildren(): Thenable<StackOverflowModel[]> {
-		let menuArray: StackOverflowModel[] = []
+		const menuArray: StackOverflowModel[] = []
 		if (this.userQuery === "" || this.userQuery === undefined) {
 			menuArray.push(this.returnNoResultsFound())
 			return Promise.resolve(menuArray)
-		} else {
-			return new Promise((resolve, reject) => {
-				let query = encodeURIComponent(this.userQuery)
-				let config = vscode.workspace.getConfiguration(
-					"Intrasight",
-					vscode.window.activeTextEditor.document.uri,
-				)
-				let numDisplayResults = config.get(
-					"StackOverflowNumberOfDisplayedResults",
-					15,
-				)
-				https.get(
-					`https://api.stackexchange.com/2.2/search?page=1&pagesize=${numDisplayResults}&order=desc&sort=relevance&intitle=${query}&site=stackoverflow`,
-					res => {
-						let raw = ""
-						let gunzip = zlib.createGunzip()
-						res.pipe(gunzip)
-						gunzip
-							.on("data", data => {
-								raw += data
-							})
-							.on("end", () => {
-								let { items } = JSON.parse(raw)
-								if (items.length !== 0) {
-									for (let arrIndex in items) {
-										menuArray.push(
-											new StackOverflowModel(
-												items[arrIndex]["title"],
-												"stackoverflow.png",
-												vscode.TreeItemCollapsibleState.None,
-												items[arrIndex]["link"],
-												{
-													command: "StackOverflow.launch",
-													title: "",
-													arguments: [items[arrIndex]["link"]],
-												},
-											),
-										)
-									}
-								} else {
-									menuArray.push(this.returnNoResultsFound())
-								}
-								resolve(menuArray)
-							})
-							.on("error", err => {
-								reject(err)
-							})
-					},
-				)
-			})
 		}
+		return new Promise((resolve, reject) => {
+			const query = encodeURIComponent(this.userQuery)
+			const config = vscode.workspace.getConfiguration(
+				"Intrasight",
+				vscode.window.activeTextEditor.document.uri,
+			)
+			const numDisplayResults = config.get(
+				"StackOverflowNumberOfDisplayedResults",
+				15,
+			)
+			const url = `https://api.stackexchange.com/2.2/search?page=1&pagesize=${numDisplayResults}&order=desc&sort=relevance&intitle=${query}&site=stackoverflow`
+			https.get(url, res => {
+				const gunzip = zlib.createGunzip()
+				res.pipe(gunzip)
+				let raw = ""
+				gunzip
+					.on("data", data => (raw += data))
+					.on("end", () => {
+						const { items } = JSON.parse(raw)
+						if (items.length !== 0) {
+							for (const arrIndex in items) {
+								menuArray.push(
+									new StackOverflowModel(
+										items[arrIndex].title,
+										"stackoverflow.png",
+										vscode.TreeItemCollapsibleState.None,
+										items[arrIndex].link,
+										{
+											command: "StackOverflow.launch",
+											title: "",
+											arguments: [items[arrIndex].link],
+										},
+									),
+								)
+							}
+						} else {
+							menuArray.push(this.returnNoResultsFound())
+						}
+						resolve(menuArray)
+					})
+					.on("error", err => {
+						reject(err)
+					})
+			})
+		})
 	}
 
 	returnNoResultsFound(): StackOverflowModel {
@@ -85,21 +76,14 @@ export class StackOverflowProvider
 }
 
 class StackOverflowModel extends vscode.TreeItem {
-	url?: string
-	icon: string
 	constructor(
-		label: string,
-		icon: string,
-		collapsibleState: vscode.TreeItemCollapsibleState,
-		url?: string,
-		command?: vscode.Command,
+		public readonly label: string,
+		public readonly icon: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly url?: string,
+		public readonly command?: vscode.Command,
 	) {
 		super(label, collapsibleState)
-		this.label = label
-		this.icon = icon
-		this.url = url
-		this.collapsibleState = collapsibleState
-		this.command = command
 		this.iconPath = {
 			dark: path.join(__filename, "..", "..", "Media", "Dark", this.icon),
 			light: path.join(__filename, "..", "..", "Media", "Light", this.icon),
