50ef23e872755aa8878105a18b455eae37d35a1b
JK <u.sslooky@gmail.com>
2021-02-12 02:29:46 +0000

    Unescape search results.
    Signed-off-by: JK <u.sslooky@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,31 @@
 import * as path from "path"
 
 export function getIconPath(filename: string, theme: "Dark" | "Light") {
-    return path.join(__filename, `../../Media/${theme}/${filename}`)
+	return path.join(__filename, `../../Media/${theme}/${filename}`)
+}
+
+// Map of HTML character entities
+const entities = {
+	amp: "&",
+	lt: "<",
+	gt: ">",
+	quot: `"`,
+	"#0x27": "\\",
+	"#x27": "\\",
+	"#0x60": "`",
+	"#x60": "`",
+}
+
+export function decodeHTML(str: string) {
+	return str.replace(/&(#?\w+);?/g, (sub, arg: string) => {
+		if (<Object>entities.hasOwnProperty(arg)) {
+			return entities[arg] || sub
+		}
+		const hex = arg.search(/x/g)
+		const n = parseInt(
+			arg.substring(hex === -1 ? 1 : hex + 1),
+			hex === -1 ? 10 : 16,
+		)
+		return !isNaN(n) ? String.fromCharCode(n) : sub
+	})
 }
